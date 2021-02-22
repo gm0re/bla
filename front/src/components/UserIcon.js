95e5bfafcc9a2b964ef92628a07b54b74a768fd8
gmore <gabriel.moreira1988@gmail.com>
2021-02-21 21:20:30 -0300

    style: :lipstick: update header with UserIcon component
    add new component UserIcon \n add UserIcon to header

---
+++ front/src/components/UserIcon.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import identicon from 'identicon';
+
+const ImgWrapper = styled.div`
+  border-radius: 100%;
+  overflow: hidden;
+  width: 54px;
+  height: 54px;
+`;
+
+const UserIcon = ({ username }) => {
+  const [avatar, setAvatar] = useState();
+
+  useEffect(() => {
+    // 👷 move to backend layer.
+    identicon.generate({
+      id: username,
+      size: 54
+    }, (err, buffer) => {
+      if (err) {
+        console.error(err);
+      }
+      setAvatar(buffer);
+    });
+  }, []);
+  return (
+    <ImgWrapper>
+      <img src={avatar} />
+    </ImgWrapper>
+  )
+};
+
+UserIcon.propTypes = {
+  username: PropTypes.string
+};
+
+export default UserIcon;
