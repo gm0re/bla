2a33deca9d5e49febbf0664ae78e0168565ff3aa
gmore <gabriel.moreira1988@gmail.com>
2021-02-25 15:41:16 -0300

    feat: :lipstick: add shadow for recordings' actions buttons

--- front/src/components/RecordingActions.js
+++ front/src/components/RecordingActions.js
@@ -3,19 +3,63 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+const ActionIcon = styled(FontAwesomeIcon)`
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  display: block;
+  top: 3px;
+  left: 5px;
+`;
+
+const ActionButtonShadow = styled.div`
+  display: none;
+  position: absolute;
+  background-color: blue;
+  border-radius: 100%;
+  width: 24px;
+  height: 24px;
+  opacity: 20%;
+`;
+
+const ActionButtonElemsContainer = styled.div`
+  width: 24px;
+  height: 24px;
+  position: relative;
+`;
+
 const RecordingActionsWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
 `;
 
+const UpVotes = styled.span``;
+
 const RecordingActionButton = styled.button`
+  &:hover ${ActionButtonShadow} {
+    display: block;
+  }
+  &:hover ${ActionIcon} {
+    color: blue;
+  }
+  &:hover ${UpVotes} {
+    color: blue;
+  }
+
   margin: 4px;
+  display: flex;
   outline: none;
   border: 0;
-  background-color: Transparent;
+  background-color: transparent;
   background-repeat: no-repeat;
   cursor: pointer;
+  flex-direction: row;
+  align-items: center;
+
+  > div {
+    margin: 4px;
+  }
 `;
 
 const RecordingActions = ({
@@ -26,11 +70,14 @@ const RecordingActions = ({
   <RecordingActionsWrapper>
     {/* <RecordingActionButton>Reply</RecordingActionButton> */}
     <RecordingActionButton onClick={onVote}>
-      {`${upVotes} `}
-      <FontAwesomeIcon
-        icon={['far', 'thumbs-up']}
-        color={`${hasVoted ? 'green' : 'red'}`}
-      />
+      <UpVotes>{`${upVotes}`}</UpVotes>
+      <ActionButtonElemsContainer>
+        <ActionIcon
+          icon={['far', 'thumbs-up']}
+          color={`${hasVoted ? 'blue' : 'grey'}`}
+        />
+        <ActionButtonShadow />
+      </ActionButtonElemsContainer>
     </RecordingActionButton>
   </RecordingActionsWrapper>
 );
