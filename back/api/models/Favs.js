09fa2f6f12c2821595620ca7a149507e7404fe05
gmore <gabriel.moreira1988@gmail.com>
2021-02-26 14:38:46 -0300

    feat: :sparkles: add favs through entity

---
+++ back/api/models/Favs.js
@@ -0,0 +1,21 @@
+/**
+ * Recordings.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    userId:{
+      model:'users'
+    },
+
+    recordingId: {
+      model: 'recordings'
+    }
+  }
+
+};
+
