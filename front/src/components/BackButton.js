15291ce1679f8b1a0fdba25e9b33621584657995
gmore <gabriel.moreira1988@gmail.com>
2021-03-04 18:15:44 -0300

    feat: :lipstick: improve a little bit the interaction with the back button

--- front/src/components/BackButton.js
+++ front/src/components/BackButton.js
@@ -8,15 +8,29 @@ const GoBackIcon = styled(FontAwesomeIcon)`
   height: 19px;
 `;
 
+const GoBackButton = styled.button`
+  &:hover  {
+    border: 1px black solid;
+    border-radius: 10%;
+    background-color: #f1f1f1;
+  }
+  margin: 4px;
+  outline: none;
+  border: 0;
+  background-color: transparent;
+  background-repeat: no-repeat;
+  cursor: pointer;
+`;
+
 const BackButton = ({ history }) => {
   const goBack = () => {
     history.goBack();
   };
 
   return (
-    <button onClick={goBack}>
+    <GoBackButton onClick={goBack}>
       <GoBackIcon icon={['fas', 'chevron-left']} />
-    </button>
+    </GoBackButton>
   )
 };
 
