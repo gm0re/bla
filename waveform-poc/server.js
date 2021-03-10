af306fc0c7300050d7be7590c22c746e11472c7f
gmore <gabriel.moreira1988@gmail.com>
2021-03-10 15:28:59 -0300

    feat: :rocket: add waveform poc

--- waveform-poc/server.js
+++ waveform-poc/server.js
@@ -1,17 +1,40 @@
+const express = require('express');
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+const engine = require('consolidate');
 
-function handleRequests(req, res) {
+const app = express();
 
+app.set('views', __dirname + '/views');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
+
+app.get('/', (_, res) => {
   fs.readFile('./sktwi1e0bkm16faiz.webm', (err, file) => {
     if (err) throw new err;
-    console.log(file.toJSON());
+
+    const { data: audioSamples } = file.toJSON();
+
+    fs.writeFile('./file.json', JSON.stringify(file.toJSON()), (err) => {
+      if (err) throw err;
+      console.log(`The file has been saved! File data buffer: ${file.toJSON().data.length}`);
+
+      const data = {
+        name: 'gmore',
+        audioSamples: audioSamples
+      };
+
+      res.render(path.join('index.html'), data, (err, html) => {
+        if (err) throw new err;
+
+        res.send(html);
+      });
+    });
   });
+});
 
-  res.end('Hello gato pato rata!');
-};
-
-const server = http.createServer(handleRequests);
+const server = http.createServer(app);
 
 server.listen(8080, () => {
   console.log('Listening on port 8080');
