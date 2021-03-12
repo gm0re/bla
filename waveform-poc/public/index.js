6356c2ce663856966f3d12a8bcf32406659b7fbf
gmore <gabriel.moreira1988@gmail.com>
2021-03-12 15:05:42 -0300

    refactor: :recycle: refactor waveform player code
    refactor player code, events listeners improved, and default audio element removed

--- waveform-poc/public/index.js
+++ waveform-poc/public/index.js
@@ -1,12 +1,11 @@
 ((() => {
-  const audioSamplesRaw = document.getElementById("audioSamples").innerHTML;
-  const audioSamples = audioSamplesRaw.split(',');
+  const audioSamples = document.getElementById("audioSamples").innerHTML.split(',');
   const canvasContainer = document.getElementById('canvas-container');
   const positionMask = document.getElementById('position-mask');
   const progressMask = document.getElementById('background');
 
   // weird stuff to get audio duration on chrome
-  const getAudioMetadata = (player, next) => {
+  const getPlayerStats = (player, next) => {
     player.addEventListener('durationchange', () => {
       if (player.duration !== Infinity) {
         player.currentTime = 0;
@@ -15,14 +14,6 @@
     }, false);
 
     player.currentTime = 24 * 60 * 60; // fake big time
-  };
-
-  const drawPositionMask = (x) => {
-    positionMask.style.width = `${x}px`;
-  };
-
-  const drawProgressMask = (x) => {
-    progressMask.style.width = `${x * 100}%`;
   };
 
   const getFilteredSamplesByBlocks = (audioBuffer, totalWidth) => {
@@ -35,6 +26,36 @@
       filteredAudioBuffer.push(audioBuffer[i]);
     }
     return filteredAudioBuffer;
+  };
+
+  const getFormattedTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const minutesLabel = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const secondsLabel = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${minutesLabel}:${secondsLabel}`;
+  }
+
+  const updateCurrentTime = ({ layerX }) => {
+    const progressPositionPercentage = layerX / canvas.width;
+    const newCurrentTime = progressPositionPercentage * player.duration;
+
+    player.currentTime = newCurrentTime;
+  };
+
+  const drawPositionMask = (x) => {
+    positionMask.style.width = `${x}px`;
+  };
+
+  const drawProgressMask = (x, type = 'pixels') => {
+    const types = {
+      percentage: '%',
+      pixels: 'px'
+    };
+    const formatWidth = type => `${types[type] === 'px' ? x : x * 100}${types[type]}`;
+
+    progressMask.style.width = formatWidth(type);
   };
 
   const drawLineSegment = (ctx, x, y, spaceBetween, isEven) => {
@@ -68,15 +89,6 @@
     }
   };
 
-  const getFormattedTime = (secs) => {
-    const minutes = Math.floor(secs / 60);
-    const seconds = Math.floor(secs % 60);
-    const minutesLabel = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    const secondsLabel = seconds < 10 ? `0${seconds}` : `${seconds}`;
-
-    return `${minutesLabel}:${secondsLabel}`;
-  }
-
   const player = document.getElementById('audio');
   const playBtn = document.getElementById('play-btn');
   const audioTime = document.getElementById('audio-time');
@@ -97,7 +109,7 @@
 
   player.addEventListener('timeupdate', () => {
     audioTime.innerHTML = `${getFormattedTime(player.currentTime)} / ${getFormattedTime(player.duration)}`;
-    drawProgressMask(player.currentTime / player.duration);
+    drawProgressMask(player.currentTime / player.duration, 'percentage');
   });
 
   const canvas = document.querySelector("canvas");
@@ -105,13 +117,11 @@
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 
-  canvasContainer.onmousemove = ({ layerX }) => {
-    drawPositionMask(layerX);
-  };
-
-  canvasContainer.onmouseout = () => {
-    drawPositionMask(0);
-  };
+  canvasContainer.addEventListener('click', updateCurrentTime);
+  canvasContainer.addEventListener('mouseup', updateCurrentTime);
+  canvasContainer.addEventListener('mousedown', updateCurrentTime);
+  canvasContainer.addEventListener('mousemove', ({ layerX }) => drawPositionMask(layerX));
+  canvasContainer.addEventListener('mouseout', () => drawPositionMask(0));
 
   const ctx = canvas.getContext('2d');
 
@@ -120,7 +130,7 @@
   ctx.translate(0, canvas.offsetHeight / 2);
   ctx.globalCompositeOperation = "destination-out";
 
-  getAudioMetadata(player, (audioMetaData) => {
+  getPlayerStats(player, (audioMetaData) => {
     console.log(audioMetaData);
   });
 
